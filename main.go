11d6e1da0d90fc961114fa9e7cb53652b889026a
Aram Ceballos <aramgonzalez12@hotmail.com>
2024-02-02 16:05:49 -0700

    Add ws chat

--- main.go
+++ main.go
@@ -4,15 +4,26 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"strconv"
+	"sync"
 
 	"github.com/aramceballos/chat-group-server/api/routes"
 	"github.com/aramceballos/chat-group-server/pkg/channel"
 	"github.com/aramceballos/chat-group-server/pkg/user"
+	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 
 	_ "github.com/lib/pq"
 )
+
+type Message struct {
+	Content string `json:"content"`
+}
+type Result struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+}
 
 const (
 	dbHost     = "localhost"
@@ -37,6 +48,17 @@ func Connect() error {
 	return nil
 }
 
+type Client struct {
+	isClosing bool
+	mu        sync.Mutex
+}
+
+var channels = make(map[int64]map[*websocket.Conn]*Client)
+
+func chatProcess() {
+
+}
+
 func main() {
 	if err := Connect(); err != nil {
 		log.Fatal(err)
@@ -54,6 +76,13 @@ func main() {
 		AllowOrigins: "http://localhost:3000",
 	}))
 
+	app.Use("/chat", func(c *fiber.Ctx) error {
+		if websocket.IsWebSocketUpgrade(c) {
+			return c.Next()
+		}
+		return c.SendStatus(fiber.StatusUpgradeRequired)
+	})
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusOK).SendString("Hello world")
 	})
@@ -65,6 +94,103 @@ func main() {
 	routes.UserRouter(v1, userService)
 
 	routes.ChannelRouter(v1, channelService)
+
+	v1.Get("/chat/:channelId", websocket.New(func(c *websocket.Conn) {
+		channelId := c.Params("channelId")
+		// Cast channelId to int64
+		channelIdInt, err := strconv.ParseInt(channelId, 10, 64)
+		if err != nil {
+			log.Println("channelId error:", err)
+			return
+		}
+
+		// Create a new client
+		client := &Client{}
+
+		// Add client to the channel
+		if _, ok := channels[channelIdInt]; !ok {
+			channels[channelIdInt] = make(map[*websocket.Conn]*Client)
+		}
+		channels[channelIdInt][c] = client
+
+		// Remove client from the channel
+		defer func() {
+			delete(channels[channelIdInt], c)
+		}()
+
+		for {
+			msg := Message{}
+			err := c.ReadJSON(&msg)
+			if err != nil {
+				errorMessage := Result{
+					Success: false,
+					Message: err.Error(),
+				}
+				err = c.WriteJSON(errorMessage)
+				if err != nil {
+					log.Println("write error:", err)
+					break
+				}
+				continue
+			}
+
+			// Check if message is empty
+			if msg.Content == "" {
+				continue
+			}
+
+			// Insert message into database
+			_, err = db.Exec("INSERT INTO messages (channel_id, user_id, content) VALUES ($1, $2, $3)", channelId, 1, string(msg.Content))
+			if err != nil {
+				errorMessage := Result{
+					Success: false,
+					Message: err.Error(),
+				}
+				err = c.WriteJSON(errorMessage)
+				if err != nil {
+					log.Println("write error:", err)
+					break
+				}
+				continue
+			}
+
+			res := Result{
+				Success: true,
+				Message: "Message sent successfully",
+			}
+			err = c.WriteJSON(res)
+			if err != nil {
+				log.Println("write error:", err)
+				break
+			}
+
+			// Send message to all clients
+			for conn, cl := range channels[channelIdInt] {
+				// Check if the client is the sender
+				if cl == client {
+					continue
+				}
+
+				go func(conn *websocket.Conn, cl *Client) {
+					cl.mu.Lock()
+					defer cl.mu.Unlock()
+					if cl.isClosing {
+						return
+					}
+
+					err = conn.WriteJSON(msg)
+					if err != nil {
+						cl.isClosing = true
+						log.Println("write error:", err)
+
+						conn.WriteMessage(websocket.CloseMessage, []byte{})
+						conn.Close()
+					}
+				}(conn, cl)
+			}
+
+		}
+	}))
 
 	app.Listen(":4000")
 }
